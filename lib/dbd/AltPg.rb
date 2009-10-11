b9132df5253f02f926f4efb0c3df82a19ce6bb1d
pilcrow <mjp@pilcrow.madison.wi.us>
2009-10-11 14:04:28 -0500

    (AltPg.rb) Refactor parameter translation, correct boolean conversion.

--- lib/dbd/AltPg.rb
+++ lib/dbd/AltPg.rb
@@ -11,6 +11,16 @@ module DBD
     def self.driver_name
       "AltPg"
     end
+
+    def self.translate_parameters(query) # :nodoc:
+      ps = DBI::SQL::PreparedStatement.new(nil, query)
+      nparams = ps.unbound.size
+      if nparams > 0
+        query = ps.bind( (1..nparams).map {|i| "$#{i}"} )
+      end
+      return [query, nparams]
+    end
+
   end # -- module AltPg
 
 end # -- module DBD
@@ -19,8 +29,14 @@ end # -- module DBI
 DBI::TypeUtil.register_conversion(DBI::DBD::AltPg.driver_name) do |obj|
   # dbi-0.4.3 String default conversion broken for native binding, since
   # the former adds quotes, which should be done by native binding
-  cascade = ! obj.is_a?(::String)
-  [obj, cascade]
+  case obj
+  when ::String
+    [obj, false]
+  when ::TrueClass, ::FalseClass
+    [obj.to_s, false]
+  else
+    [obj, true]
+  end
 end
 
 require 'dbd/altpg/driver'
