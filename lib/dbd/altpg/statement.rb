5decc1a4ce6b08d6107ab415ea53336257fff8ec
pilcrow <mjp@pilcrow.madison.wi.us>
2009-10-31 00:37:14 -0500

    (statement.rb) Perform connection-specific inbound parameter conversion (which register_conversion cannot)

--- lib/dbd/altpg/statement.rb
+++ lib/dbd/altpg/statement.rb
@@ -22,4 +22,32 @@ class DBI::DBD::AltPg::Statement < DBI::BaseStatement
     (@attr ||= {})[key] = value
   end
 
+  def bind_param(i, value, extra)
+    translated = case value
+                 when nil
+                   [nil, :unknown, 0]
+                 when TrueClass
+                   #["\1", :bool, 1 ]
+                   ["t", :bool, 0 ]
+                 when FalseClass
+                   #["\0", :bool, 1 ]
+                   ["f", :bool, 0 ]
+                 when String
+                   [ value, :varchar, 1 ]
+                 #when DBI::DBD::AltPg::Type::ByteA
+                 #  [ value, :bytea, 1 ]
+                 when ::Date
+                   [ value.strftime('%Y-%m-%d'), :date, 0 ]
+                 when ::Time, ::DateTime
+                   [ value.strftime("%Y-%m-%dT%H:%M:%S%Z"), :timestamptz, 0 ]
+                 when BigDecimal
+                   [ value.to_s('F'), :numeric, 0 ]
+                 when Numeric
+                   [ value.to_s, :numeric, 0 ]
+                 else
+                   [ value.to_s, :unknown, 0 ]
+                 end
+    translated[1] = @type_map[ translated[1] ][:oid]
+    @params[i - 1] = translated
+  end
 end #-- class DBI::DBD::AltPg::Statement
